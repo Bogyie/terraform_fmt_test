c27c05b51f518f821ae9586c39b3b59f389afca6
김보경 <bogyeong@lguplus.co.kr>
2023-05-14 10:43:53 +0900

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   asdf             = "12341234"
-  ixxx             = "12341234"
-  asdfff           = "asdf"
+  ixxx      = "12341234"
+  asdfff          = "asdf"
   ii               = 1
   asdfsadfasdfasdf = "1234"
-  _a               = 0
+  _a        = 0
 }
